f024270af921c6cd31c62fb03b7e9d505b3b9cbf
Alexander Kneipp <alexander.h.kneipp@gmail.com>
2019-10-01 20:39:43 -0400

    Merge from upstream

--- src/subsystems/swerveDrive.cpp
+++ src/subsystems/swerveDrive.cpp
@@ -1,8 +1,22 @@
 #include "subsystems/swerveDrive.hpp"
 #include "main.h"
+#include <math.h>
+
+//Control algorithm based on papers found at this link:
+//https://www.chiefdelphi.com/t/paper-4-wheel-independent-drive-independent-steering-swerve/107383
 
 void SwerveDrive::drive(float direction, float speed, float rotation)
 {
+  float dirRads = direction * (PI/180);
+  //the X axis is defined so the robot strafing towards the starboard direction
+  //is positive x
+  float x = speed * (cos( dirRads ));
+  //The Y axis is defined so the robot traveling in the forward direction is
+  //positive y
+  float y = speed * (sin( dirRads ));
+  //The z axis is defined using the right hand rule relative to the robots rotation
+  float z = -rotation;
+
 
 }
 
@@ -28,5 +42,5 @@ bool SwerveDrive::auto_drive_inches(float direction, float speed)
 
 bool SwerveDrive::auto_rotate_degrees(float degrees, float speed)
 {
-  
+
 }
