a8ac7a50c2da39f84e3d161dc58e22cc72845350
Dana <dec1701@rit.edu>
2020-02-09 14:45:23 -0500

    Fixed build issues

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -22,56 +22,7 @@ enum COLOR {BLUE = 1, RED = -1};
 
 COLOR curr_color = BLUE;
 
-/*
-* Jostles the robot front & back
-* to flip the intake out
-*/
-void intake_out(){
-  while(drive_system.drive_forward(1, 1)){}
-  while(drive_system.drive_forward(-1, 1)){}
-}
-
-/*
-*
-*/
-void hold_lift(void* pos){
-  float* pos_val = (float*)pos;
-  while(true){
-    lift.hold_pos(*pos_val);
-  }
-}
-
-/*
-* Places a cube in the tower using the button
-*   to line up to the tower
-* @precondition: Robot has a cube in its claw
-*/
-void tower_place(float tower_height){
-  //Lift to tower height
-  Task hold_lift_task(hold_lift, &tower_height);
-
-  //Records time when the function was called
-  timer.placeMark();
-
-  //Lining up w/ the tower
-  while(timer.getDtFromMark().getValue() < 1000 && !Hardware::tower_button.get_value()){
-    drive_system.drive(60, 60);
-  }
-
-  //Opening the claw to drop the cube in the tower
-  while(!claw.open()){}
-
-  hold_lift_task.remove();
-}
-
 //Aim: one cube in center tower closest to starting squares
 void autonomous(){
-  //Starting position: on starting square furthest from drivers
-  intake_out();
-  while(!drive_system.drive_forward(24, 0.75)){}
-  while(!drive_system.turn_degrees(curr_color * 90, 0.5)){}
-  while(!claw.open()){}
-  while(!drive_system.drive_forward(15, 0.75)){}
-  while(!claw.close()){}
-  tower_place(0.8);
+  
 }
